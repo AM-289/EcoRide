d05d57c4fa5d3d17bbd7782ae48db5dcf5e15857
AM-289 <abbi.murcia@gmail.com>
2025-08-20 10:42:13 +0200

    mail

---
+++ Test/src/Controller/RideController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ride;
+use App\Form\RideType;
+use App\Repository\RideRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/trajet', name: 'ride')]
+final class RideController extends AbstractController
+{
+    
+    #[Route('/', name: '.search.result')]
+    public function index(Request $request, RideRepository $rideRepository): Response
+    {
+        $rides = $rideRepository->findAll();
+
+        return $this->render('ride/index.html.twig', [
+            'controller_name' => 'RideController',
+            'rides' => $rides
+        ]);
+    }
+
+    #[Route('/{slug}-{id}', name: '.details', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function rideDetails(Request $request, Ride $ride): Response {
+        return $this->render('ride/ride.details.html.twig', [
+            'ride' => $ride
+        ]);
+    }
+
+    #[Route('/{slug}-{id}/editer', name: '.edit', methods: ['GET', 'POST'], requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function editRide (Ride $ride, Request $request, EntityManagerInterface $em) {
+        $form = $this->createForm(RideType::class, $ride);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Les modifications ont bien été enregistrée');
+            return $this->redirectToRoute('ride.search.result');
+        }
+        return $this->render('ride/edit.html.twig', [
+            'ride' => $ride,
+            'rideForm' => $form
+        ]);
+    }
+
+    #[Route('/creer_trajet', name : '.create')]
+    public function create(Request $request, EntityManagerInterface $em) {
+        $ride = new Ride();
+        $form = $this->createForm(RideType::class, $ride);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($ride);
+            $em->flush();
+            $this->addFlash('success', 'Trajet créé');
+            return $this->redirectToRoute('ride.search.result');
+        }
+        return $this->render('ride/create.html.twig', [
+            'rideForm' => $form
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: '.delete', methods: ['DELETE'])]
+    public function removeRide(Ride $ride, EntityManagerInterface $em) {
+        $em->remove($ride);
+        $em->flush();
+        $this->addFlash('success', 'Tajet supprimé');
+        return $this->redirectToRoute('ride.search.result');
+    }
+}
